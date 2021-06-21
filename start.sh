0b4e515dca129224ccc332d0359b97fe3b2b02b4
aleegreat <aleegreat@gmail.com>
2021-06-21 20:16:13 +0800

    auto

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ echo "step1 start kite..............."
 su - abc -c "/home/abc/.local/share/kite/kited " &
 
 echo "step2 tail..............."
-/home/abc/Anaconda3/bin/jupyter-lab "$*"
-
+# /home/abc/Anaconda3/bin/jupyter-lab "$*"
+tail -f /dev/null
